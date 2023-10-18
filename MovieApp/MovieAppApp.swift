54ff4091bdb569ad33efffb39bca5ecc2ec7ea5e
Aman Gupta <aman.gupta3@publicissapient.com>
2023-10-18 13:30:18 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Aman Gupta on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
